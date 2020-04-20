46c5493c753ab6860dcf9a470ef880037f12783f
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-20 06:29:08 +0200

    NavBar with 3 first pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+// Components
+import NavBar from "./components/Navbar";
 
 // Pages
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
 function App() {
   return (
     <div className="App">
       <Router>
-    <Switch>
-      <Route exact path="/" component={home}/>
-      <Route exact path="/login" component={login}/>
-      <Route exact path="/signup" component={signup}/>
-    </Switch>
+          <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
